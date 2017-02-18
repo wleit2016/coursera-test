d5ba1c8051fefaa19ffa18a005c2a2c0dd66d614
wleit2016 <wenderleite2008@gmail.com>
2017-02-17 22:13:54 -0200

    Add files via upload

---
+++ ola_mundo.js
@@ -0,0 +1,6 @@
+var http = require('http');
+http.createServer(function(req,res){
+	res.writeHead(200, {'Content-Type': 'text/html; charset=utf-8'});
+	res.end('<h2>Ola mundo!</h2>');
+}).listen(3000);
+console.log('Servidor iniciado em localhost:3000. Ctrl+C para encerrar...')
